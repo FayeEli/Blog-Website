9a417acd1114f6eb9dfcd716f07e3ac3f8911c31
FayeEli <159393071+FayeEli@users.noreply.github.com>
2024-05-13 11:31:24 -0700

    update of blog css

--- blog.js
+++ blog.js
@@ -8,8 +8,26 @@ var flkty = new Flickity( elem, {
   imagesLoaded: true,
 });
 
+var elem = document.querySelector('.slider');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  pageDots: false,
+  wrapAround: true,
+  imagesLoaded: true,
+  prevNextButtons: false,
+});
+
 const save = document.querySelector('.save');
+const burger = document.querySelector('.burger');
+const burger = document.querySelector('.logo a');
 
 save.addEventListener("click", function(){
     this.classlist.toggle("active")
+})
+
+burger.addEventListener("click", function(){
+    this.classlist.toggle("active");
+    logo.classlist.toggle("active");
 })
