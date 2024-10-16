fd6f619c8746ffb2e807a44afc5b889b5f7b0917
FayeEli <159393071+FayeEli@users.noreply.github.com>
2024-10-15 19:18:52 -0700

    Add files via upload
    Updated code folder added

---
+++ FayeEli-Blog-Website-c8a63eb/blog-post.js
@@ -0,0 +1,11 @@
+function shareOnTwitter() {
+    alert('Shared on Twitter');
+}
+
+function shareOnFacebook() {
+    alert('Shared on Facebook');
+}
+
+function shareOnLinkedIn() {
+    alert('Shared on LinkedIn');
+}
